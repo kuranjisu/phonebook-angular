7ab56963343d0c577ecb7bf7e3d0ad6bcbd8bca9
Aj <arvi.laco@novare.com.hk>
2023-02-21 10:41:47 +0800

    initial commit

---
+++ src/app/mock-data/mock-contacts.ts
@@ -0,0 +1,9 @@
+import { Contact } from "../interfaces/contact";
+
+export const CONTACTS: Contact[] = [
+    {name: 'Aj', email: 'aj@test.com', number: '123'},
+    {name: 'Bj', email: 'bj@test.com', number: '456'},
+    {name: 'Cj', email: 'cj@test.com', number: '789'},
+    {name: 'Dj', email: 'dj@test.com', number: '321'},
+    {name: 'Ej', email: 'ej@test.com', number: '654'},
+];
